561fbc14fd619b351266ab57861ce94d2b918de6
Yannic Rieger <riegeryannic@gmail.com>
2021-06-05 17:23:43 +0200

    write build script and add missing blueprint files

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+set -e
 
-# TODO: check if image version already exists in repo, if so, skip
-# TODO: build base
-# TODO: build all other stuff
+function build() {
+  version=$(cat $1/blueprint.json | jq -r .version)
+  name=$(cat $1/blueprint.json | jq -r .name)
+  tag=ghcr.io/cownetwork/$name:$version
+  exists=$(docker manifest inspect $tag > /dev/null ; echo $?)
+
+  if [ $exists == 0 ]; then   
+    echo "$tag already exists. skipping."
+    return
+  fi
+    
+  docker build -t $tag -f $1/Dockerfile $1
+  docker push $tag
+}
+
+docker login ghcr.io -u $DOCKER_REPO_USER -p $DOCER_REPO_PASS
+
+build base
+
+for dir in */ ; do
+  mode=$(basename $dir)
+  if [ "$mode" == "base" ]; then
+    continue
+  fi
+  build $mode
+done
